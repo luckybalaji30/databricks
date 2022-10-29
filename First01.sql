ea50dc307fb84cc1adb9eaaad2a6fd81523a46d8
luckybalaji30@gmail.com <luckybalaji30@gmail.com>
2022-10-29 03:43:54 +0000

    Created Table and first set of row

--- First01.sql
+++ First01.sql
@@ -4,3 +4,8 @@ create database if not exists company;
 -- COMMAND ----------
 
 create if not exist employee (emp_id int, emp_name varchar(20))
+insert into
+  employee
+values
+  (100, 'Balaji'),
+  (200, 'Suji');
